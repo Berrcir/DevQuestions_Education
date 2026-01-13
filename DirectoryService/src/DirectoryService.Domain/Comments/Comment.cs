26d8ac78ecd0fb0b9efb0e44a0ba127afae34f46
Berrcir <berciforever123@gmail.com>
2026-01-13 21:14:38 +0700

    Feature: Domain models created

---
+++ DirectoryService/src/DirectoryService.Domain/Comments/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace DirectoryService.Domain.Comments
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public required Guid UserId { get; set; }
+        public required string Text { get; set; }
+        public required Guid Parent { get; set; }
+        public required string ParentType { get; set; }
+    }
+}
---
+++ DirectoryService/src/DirectoryService.Domain/Questions/Answer.cs
@@ -0,0 +1,10 @@
+﻿namespace DirectoryService.Domain.Questions
+{
+    public class Answer
+    {
+        public Guid Id { get; set; }
+        public required Guid UserId{ get; set; }
+        public required string Text { get; set; }
+        public required Question Question { get; init; }
+    }
+}
